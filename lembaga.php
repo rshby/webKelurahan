b44991df1a275f13d7bcf41c1b9313adbb623aa6
rshby <reoshby@gmail.com>
2020-01-21 20:20:50 +0700

    Update lembaga.php

--- lembaga.php
+++ lembaga.php
@@ -16,7 +16,7 @@ $data = $data['data'];
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="shortcut icon" href="pemkot.png"/>
-  <script type="text/javascript" src="Chart.js"></script>
+  <script type="text/javascript" src="assets/Chart.js"></script>
   <title>Lembaga</title>
 </head>
 <body>
