91729afb02803e5d669527619dea2b96e1c07be6
rshby <reoshby@gmail.com>
2020-01-22 14:06:26 +0700

    Update gambaranumum.php
    menghapus lorem lorem tidak terpakai

--- gambaranumum.php
+++ gambaranumum.php
@@ -124,12 +124,7 @@
 					<h4 class="tulisan-menu">Tematik dan Potensi Wilayah</h4>
 				</div>
 				<div class="isi">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					
 				</div>   
 			</div>
 
